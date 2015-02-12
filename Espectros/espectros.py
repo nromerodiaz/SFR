dcc6d4a2e186383cd509b4380ec9a295359f56b3
nromerodiaz <n.romero1661@uniandes.edu.co>
2015-02-12 17:14:25 -0500

    declaracion de importaciones

--- Espectros/espectros.py
+++ Espectros/espectros.py
@@ -1,1 +1,33 @@
 # Programa que permite extraer espectros de pixeles solicitados
+#
+#
+#------------------------IFSview.py----------------------------
+#
+#
+#import visvis as vv
+from matplotlib.widgets import Cursor
+from tkFileDialog import askopenfilename
+import matplotlib
+matplotlib.use('TkAgg')
+
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+
+from matplotlib.colors import colorConverter
+import Tkinter as Tk
+import sys
+
+def destroy(e): sys.exit()
+
+import sys
+import pyfits
+import matplotlib
+#matplotlib.use('TkAgg')
+
+import matplotlib.pyplot as plt
+#plt.jet()
+import numpy as np
+from mpl_toolkits.mplot3d  import Axes3D
+import math, copy
+from matplotlib import pyplot, colors, cm
